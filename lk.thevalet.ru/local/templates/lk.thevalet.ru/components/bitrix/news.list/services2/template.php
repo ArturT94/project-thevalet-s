3a88b0b98da45c42452718506d6d040c2fa36759
ArturT94 <gunsmoke94@mail.ru>
2022-06-21 15:03:14 +0300

    Добавление компонентов услуги и сервис

---
+++ lk.thevalet.ru/local/templates/lk.thevalet.ru/components/bitrix/news.list/services2/template.php
@@ -0,0 +1,39 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+/** @var array $arParams */
+/** @var array $arResult */
+/** @global CMain $APPLICATION */
+/** @global CUser $USER */
+/** @global CDatabase $DB */
+/** @var CBitrixComponentTemplate $this */
+/** @var string $templateName */
+/** @var string $templateFile */
+/** @var string $templateFolder */
+/** @var string $componentPath */
+/** @var CBitrixComponent $component */
+$this->setFrameMode(true);
+?>
+<!--Весь фрейм услуг на экране совершения заказа-->
+    <!--Карточки услуг "VALET-СЕРВИС"-->
+    <h2 class="chapter__title"><?=$arResult['ITEMS'][1]['NAME']?></h2>
+    <div class="nav_group"> <!--Пагинация и навигация в слайдере-->
+        <div class="swiper-button-prev sw-Btn-service2-Prev"></div>
+        <div class="swiper-button-next sw-Btn-service2-Next"></div>
+    </div>
+<?php //echo '<pre>' . print_r($arResult['ITEMS'], true) . '</pre>';?>
+    <div class="swiper vltServise2">
+        <div class="swiper-wrapper">
+
+            <?php foreach($arResult["ITEMS"] as $arItems): ?>
+                <?php foreach($arItems['ELEMENTS'] as $arItem): ?>
+            <button class="swiper-slide vlt_servise">
+                <a href="#popup__service" class="service__icon">
+                    <img src="<?=SITE_TEMPLATE_PATH?>/img/layout/services-info.svg" width="26" height="26" alt="">
+                </a>
+                <?php $imgValServes = CFile::GetPath($arItem['PROPERTIES']['FOTO']['VALUE']); ?>
+                <img class="vlt_servise_img" draggable=false src="<?=$imgValServes?>" width="200px" height="150px" alt="">
+                <p class="servise__name"><?=strtoupper($arItem['NAME'])?></p>
+            </button>
+                <?php endforeach; ?>
+            <?php endforeach; ?>
+        </div>
+    </div>
--- lk.thevalet.ru/newOrder.php
+++ lk.thevalet.ru/newOrder.php
@@ -141,7 +141,139 @@ $APPLICATION->SetTitle(""); ?>
 	)
 );?> <!--Фрейм списка автомобилей-->
 			</div>
-			 <?php include "serviceListOnOrder.php"; ?> <!--Фрейм списка услуг--> <section class="order-button"> <button type="button" class="button__order js-button-order" disabled=""><a href="#addition" class="order__popup__link">выберите услугу</a></button> <button type="button" class="button__order js-button-order button__hide js-button-next">далее</button> </section>
+            <div class="service">
+            <?$APPLICATION->IncludeComponent(
+	"bitrix:news.list", 
+	"services", 
+	array(
+		"COMPONENT_TEMPLATE" => "services",
+		"IBLOCK_TYPE" => "lists",
+		"IBLOCK_ID" => "95",
+		"NEWS_COUNT" => "20",
+		"SORT_BY1" => "ACTIVE_FROM",
+		"SORT_ORDER1" => "DESC",
+		"SORT_BY2" => "SORT",
+		"SORT_ORDER2" => "ASC",
+		"FILTER_NAME" => "",
+		"FIELD_CODE" => array(
+			0 => "NAME",
+			1 => "",
+		),
+		"PROPERTY_CODE" => array(
+			0 => "INFORMATSIYA_OB_USLUGE",
+			1 => "FOTO",
+			2 => "",
+			3 => "",
+		),
+		"CHECK_DATES" => "Y",
+		"DETAIL_URL" => "",
+		"AJAX_MODE" => "N",
+		"AJAX_OPTION_JUMP" => "N",
+		"AJAX_OPTION_STYLE" => "Y",
+		"AJAX_OPTION_HISTORY" => "N",
+		"AJAX_OPTION_ADDITIONAL" => "",
+		"CACHE_TYPE" => "N",
+		"CACHE_TIME" => "36000000",
+		"CACHE_FILTER" => "N",
+		"CACHE_GROUPS" => "Y",
+		"PREVIEW_TRUNCATE_LEN" => "",
+		"ACTIVE_DATE_FORMAT" => "d.m.Y",
+		"SET_TITLE" => "Y",
+		"SET_BROWSER_TITLE" => "Y",
+		"SET_META_KEYWORDS" => "Y",
+		"SET_META_DESCRIPTION" => "Y",
+		"SET_LAST_MODIFIED" => "N",
+		"INCLUDE_IBLOCK_INTO_CHAIN" => "Y",
+		"ADD_SECTIONS_CHAIN" => "Y",
+		"HIDE_LINK_WHEN_NO_DETAIL" => "N",
+		"PARENT_SECTION" => "",
+		"PARENT_SECTION_CODE" => "",
+		"INCLUDE_SUBSECTIONS" => "Y",
+		"STRICT_SECTION_CHECK" => "N",
+		"DISPLAY_DATE" => "Y",
+		"DISPLAY_NAME" => "Y",
+		"DISPLAY_PICTURE" => "Y",
+		"DISPLAY_PREVIEW_TEXT" => "Y",
+		"PAGER_TEMPLATE" => ".default",
+		"DISPLAY_TOP_PAGER" => "N",
+		"DISPLAY_BOTTOM_PAGER" => "Y",
+		"PAGER_TITLE" => "Новости",
+		"PAGER_SHOW_ALWAYS" => "N",
+		"PAGER_DESC_NUMBERING" => "N",
+		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+		"PAGER_SHOW_ALL" => "N",
+		"PAGER_BASE_LINK_ENABLE" => "N",
+		"SET_STATUS_404" => "N",
+		"SHOW_404" => "N",
+		"MESSAGE_404" => ""
+	),
+	false
+);?>
+            <?$APPLICATION->IncludeComponent("bitrix:news.list", "services2", Array(
+	"COMPONENT_TEMPLATE" => "services",
+		"IBLOCK_TYPE" => "lists",	// Тип информационного блока (используется только для проверки)
+		"IBLOCK_ID" => "95",	// Код информационного блока
+		"NEWS_COUNT" => "20",	// Количество новостей на странице
+		"SORT_BY1" => "ACTIVE_FROM",	// Поле для первой сортировки новостей
+		"SORT_ORDER1" => "DESC",	// Направление для первой сортировки новостей
+		"SORT_BY2" => "SORT",	// Поле для второй сортировки новостей
+		"SORT_ORDER2" => "ASC",	// Направление для второй сортировки новостей
+		"FILTER_NAME" => "",	// Фильтр
+		"FIELD_CODE" => array(	// Поля
+			0 => "NAME",
+			1 => "",
+		),
+		"PROPERTY_CODE" => array(	// Свойства
+			0 => "INFORMATSIYA_OB_USLUGE",
+			1 => "FOTO",
+			2 => "",
+		),
+		"CHECK_DATES" => "Y",	// Показывать только активные на данный момент элементы
+		"DETAIL_URL" => "",	// URL страницы детального просмотра (по умолчанию - из настроек инфоблока)
+		"AJAX_MODE" => "N",	// Включить режим AJAX
+		"AJAX_OPTION_JUMP" => "N",	// Включить прокрутку к началу компонента
+		"AJAX_OPTION_STYLE" => "Y",	// Включить подгрузку стилей
+		"AJAX_OPTION_HISTORY" => "N",	// Включить эмуляцию навигации браузера
+		"AJAX_OPTION_ADDITIONAL" => "",	// Дополнительный идентификатор
+		"CACHE_TYPE" => "A",	// Тип кеширования
+		"CACHE_TIME" => "36000000",	// Время кеширования (сек.)
+		"CACHE_FILTER" => "N",	// Кешировать при установленном фильтре
+		"CACHE_GROUPS" => "Y",	// Учитывать права доступа
+		"PREVIEW_TRUNCATE_LEN" => "",	// Максимальная длина анонса для вывода (только для типа текст)
+		"ACTIVE_DATE_FORMAT" => "d.m.Y",	// Формат показа даты
+		"SET_TITLE" => "Y",	// Устанавливать заголовок страницы
+		"SET_BROWSER_TITLE" => "Y",	// Устанавливать заголовок окна браузера
+		"SET_META_KEYWORDS" => "Y",	// Устанавливать ключевые слова страницы
+		"SET_META_DESCRIPTION" => "Y",	// Устанавливать описание страницы
+		"SET_LAST_MODIFIED" => "N",	// Устанавливать в заголовках ответа время модификации страницы
+		"INCLUDE_IBLOCK_INTO_CHAIN" => "Y",	// Включать инфоблок в цепочку навигации
+		"ADD_SECTIONS_CHAIN" => "Y",	// Включать раздел в цепочку навигации
+		"HIDE_LINK_WHEN_NO_DETAIL" => "N",	// Скрывать ссылку, если нет детального описания
+		"PARENT_SECTION" => "196",	// ID раздела
+		"PARENT_SECTION_CODE" => "",	// Код раздела
+		"INCLUDE_SUBSECTIONS" => "Y",	// Показывать элементы подразделов раздела
+		"STRICT_SECTION_CHECK" => "N",	// Строгая проверка раздела для показа списка
+		"DISPLAY_DATE" => "Y",	// Выводить дату элемента
+		"DISPLAY_NAME" => "Y",	// Выводить название элемента
+		"DISPLAY_PICTURE" => "Y",	// Выводить изображение для анонса
+		"DISPLAY_PREVIEW_TEXT" => "Y",	// Выводить текст анонса
+		"PAGER_TEMPLATE" => ".default",	// Шаблон постраничной навигации
+		"DISPLAY_TOP_PAGER" => "N",	// Выводить над списком
+		"DISPLAY_BOTTOM_PAGER" => "Y",	// Выводить под списком
+		"PAGER_TITLE" => "Новости",	// Название категорий
+		"PAGER_SHOW_ALWAYS" => "N",	// Выводить всегда
+		"PAGER_DESC_NUMBERING" => "N",	// Использовать обратную навигацию
+		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",	// Время кеширования страниц для обратной навигации
+		"PAGER_SHOW_ALL" => "N",	// Показывать ссылку "Все"
+		"PAGER_BASE_LINK_ENABLE" => "N",	// Включить обработку ссылок
+		"SET_STATUS_404" => "N",	// Устанавливать статус 404
+		"SHOW_404" => "N",	// Показ специальной страницы
+		"MESSAGE_404" => "",	// Сообщение для показа (по умолчанию из компонента)
+	),
+	false
+);?>
+   </div>
+                <!--Фрейм списка услуг--> <section class="order-button"> <button type="button" class="button__order js-button-order" disabled=""><a href="#addition" class="order__popup__link">выберите услугу</a></button> <button type="button" class="button__order js-button-order button__hide js-button-next">далее</button> </section>
 		</div>
 	</div>
 	 <?php include "map.php"; ?> <!--Google Карта-->
