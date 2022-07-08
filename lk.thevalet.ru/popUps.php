732ecfbb51ae343e8be9693a479b765b2c6db92e
ArturT94 <gunsmoke94@mail.ru>
2022-07-08 03:47:10 +0300

    Изменения от тестового сервера на 07.07.2022

---
+++ lk.thevalet.ru/popUps.php
@@ -0,0 +1,33 @@
+<link rel="stylesheet" href="<?= SITE_TEMPLATE_PATH ?>/css/popups.css">
+
+<div class="popup" id="popup__beta">
+  <div class="popup__content">
+    <button class="menu__close__popup service__close-popup">
+      <img class="menu__close-icon" src="<?= SITE_TEMPLATE_PATH ?>/img/layout/close.png">
+    </button>
+
+    <div class="beta__main">
+      <div class="beta__title">
+        ЗДРАВСТВУЙТЕ!
+      </div>
+      <div class="beta__text">Рады видеть Вас на нашем новом сайте!
+        Пока здесь доступны не все функции и могу встречаться ошибки.</div>
+    </div>
+  </div>
+</div>
+
+<div class="popup" id="delete__car">
+  <div class="popup__content">
+    <button class="menu__close__popup service__close-popup">
+      <img class="menu__close-icon" src="<?= SITE_TEMPLATE_PATH ?>/img/layout/close.png">
+    </button>
+
+    <div class="cardel__main">
+      <div class="cardel__text">Удалить этот авто из гаража?</div>
+      <div class="cardel__buttons">
+        <button type="button" class="cardel__button">Да</button>
+        <button type="button" class="cardel__button">Нет</button>
+      </div>
+    </div>
+  </div>
+</div>
